f8323be4177d0dc0c2c5a0fc7687ee0cafe02769
Rahul <rahul.wagh17@gmail.com>
2019-01-29 21:16:48 +0200

    JHOOQ_SPRING_BOOT_2 : Spring boot module project skeleton

---
+++ spring-boot-app/src/main/java/com/jhooq/springboot/springboot/SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jhooq.springboot.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
+
+}
+
--- spring-boot-app/src/test/java/com/jhooq/springboot/springboot/SpringBootApplicationTests.java
+++ spring-boot-app/src/test/java/com/jhooq/springboot/springboot/SpringBootApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootApplicationTests {
+public class SpringBootApplicationTests {
 
 	@Test
 	public void contextLoads() {
